cdbce4337144decaf9a4ce8da365ca46b22fd230
linux <linux@linux-laptop>
2013-04-08 15:40:43 -0300

    Se campo contenido a tipoempresa

--- application/views/admin/tipoempresa/tipoempresa_form.php
+++ application/views/admin/tipoempresa/tipoempresa_form.php
@@ -35,6 +35,12 @@
                             <input type="text" class="span10"  name="DesEmpresa" value="<?php echo $DesEmpresa ?>">
                         </div>
                     </div>
+                    <div class="control-group">
+                        <label class="control-label" >Contenido:</label>
+                        <div class="controls">
+                            <textarea class="ckeditor" name="ContEmpresa"><?php echo $ContEmpresa ?></textarea>
+                        </div>
+                    </div>
                     <div class="offset8"><button class="btn btn-large btn-primary" type="submit" >Guardar</button>&nbsp;&nbsp;<a class="btn btn-large btn-info" href="<?php echo base_url() . "admin/tipoempresa/lists" ?>">Volver</a></div>
                 </div>
                 <input type="hidden" id="base_url" value="<?php echo base_url() ?>">
